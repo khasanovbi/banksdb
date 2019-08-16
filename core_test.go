4d075194424a099abbb50d363f989c53fc6c0e17
Bulat Khasanov <afti@yandex.ru>
2019-08-17 02:55:01 +0300

    Add more tests for banksDB

--- core_test.go
+++ core_test.go
@@ -10,3 +10,8 @@ func TestFindBankEmptyCreditCard(t *testing.T) {
 	bank := FindBank("")
 	require.Nil(t, bank)
 }
+
+func TestFindBankInvalidCreditCard(t *testing.T) {
+	bank := FindBank("no-digits")
+	require.Nil(t, bank)
+}
---
+++ db_test.go
@@ -0,0 +1,47 @@
+package banksdb
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type NewBanksDBTestSuite struct {
+	suite.Suite
+	country1 Country
+	bank1    *Bank
+	country2 Country
+	bank2    *Bank
+}
+
+func (suite *NewBanksDBTestSuite) getSomeBankPrefix(bank *Bank) string {
+	return strconv.Itoa(bank.Prefixes[0])
+}
+
+func (suite *NewBanksDBTestSuite) SetupSuite() {
+	suite.country1 = RU
+	suite.bank1 = &banksByCountry[suite.country1][0]
+	suite.country2 = CN
+	suite.bank2 = &banksByCountry[suite.country2][0]
+}
+
+func (suite *NewBanksDBTestSuite) TestEmptyNewBanksDBTestSuite() {
+	emptyBanksDB := NewBanksDB()
+	bank := emptyBanksDB.FindBank(suite.getSomeBankPrefix(suite.bank1))
+	suite.Require().Nil(bank)
+}
+
+func (suite *NewBanksDBTestSuite) TestFindBankInSingleCountry() {
+	banksDB := NewBanksDB(suite.country1)
+	bank := banksDB.FindBank(suite.getSomeBankPrefix(suite.bank1))
+	suite.Require().NotNil(bank)
+	suite.Require().Equal(suite.bank1, bank)
+
+	bank = banksDB.FindBank(suite.getSomeBankPrefix(suite.bank2))
+	suite.Require().Nil(bank)
+}
+
+func TestNewBanksDBTestSuite(t *testing.T) {
+	suite.Run(t, new(NewBanksDBTestSuite))
+}
